6455800b15e794c330f272ce0a8d42c2b22ec781
Michael Szmadzinski <smujmaiku@gmail.com>
2024-06-23 23:06:38 -0400

    add loose ref deps

--- src/use-loose-ref.ts
+++ src/use-loose-ref.ts
@@ -1,4 +1,4 @@
-import { useState, MutableRefObject } from 'react';
+import { useState, MutableRefObject, useEffect, useRef } from 'react';
 import useRefCallback from './use-ref-callback';
 
 export interface MutableLooseRefObject<T> extends MutableRefObject<T> {
@@ -10,17 +10,18 @@ export interface MutableLooseRefObject<T> extends MutableRefObject<T> {
  * 
  * Useful for holding jwt credentails that may refresh or expire
  * @param initialValue useRef initialValue
+ * @param deps If present, effect will only activate if the values in the list change
  * @example
  * const authRef = useLooseRef(0);
  * authRef.refresh();
  */
-export function useLooseRef<T>(initialValue: T): MutableLooseRefObject<T> {
+export function useLooseRef<T>(initialValue: T, deps = [] as React.DependencyList): MutableLooseRefObject<T> {
 	const [ref, setRef] = useState<MutableLooseRefObject<T>>({
 		current: initialValue,
 		refresh: null!
 	});
 
-	ref.refresh = useRefCallback(() => {
+	const refresh = useRefCallback(() => {
 		const newRef = { ...ref };
 
 		Object.defineProperty(ref, "current", {
@@ -35,6 +36,16 @@ export function useLooseRef<T>(initialValue: T): MutableLooseRefObject<T> {
 		setRef(newRef);
 	});
 
+	const firstRefresh = useRef(true);
+	useEffect(() => {
+		if (firstRefresh.current) {
+			firstRefresh.current = false;
+			return;
+		}
+		refresh();
+	}, [refresh, ...deps])
+
+	ref.refresh = refresh;
 	return ref;
 }
 
